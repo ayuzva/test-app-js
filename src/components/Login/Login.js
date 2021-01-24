bd45ac5b3be932f17e2e8312a73a52a3c9469591
ayuzva <yuzvaa@mcmaster.ca>
2021-01-24 11:37:53 -0500

    Add log in form handler

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,14 +13,22 @@ async function loginUser(credentials){
     }).then(data => data.json());
 }
 
-
 function Login({setToken}) {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
 
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
     return (
         <div className="login-wrapper">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
                     <input type="text" onChange={e => {setUserName(e.target.value)}}/>
